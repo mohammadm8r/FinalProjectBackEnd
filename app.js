878e4eb99ca866318e0bf25d477ebb1d0e358160
farziin <farzin.heidary@gmail.com>
2020-08-13 11:57:09 +0430

    99/05/23 11:56 => serving profile pic

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3030,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
     });
+
+    await server.register(require('@hapi/inert'));
 
     server.route({
         method: 'GET',
         path: '/',
         handler: (request, h) => {
             return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/student/pic/{std_id}',
+        handler: (request, h) => {
+            return h.file('student/' + request.params.std_id + '.jpg');
         }
     });
 
